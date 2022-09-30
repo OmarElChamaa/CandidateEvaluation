6fd34fa358186cb21331517f8cc43d267443bbb7
Omar El Chamaa <omarchamaalak@gmail.com*>
2022-09-30 08:59:16 +0200

    Version 1.0

--- plotter.py
+++ plotter.py
@@ -33,16 +33,18 @@ class Candidat :
                     jsonStr = dict 
                     jsonStr =  json.dumps(jsonStr)
                 else: #il faudra tester ici si la tale existe deja ou pas 
-                    if self.test in dict :
+                    jsonStr = json.load(infile)
+                    if self.test in jsonStr :
                         print("The table",self.test,"is in the dictionnary")
                         dict = {
                             "Name" : self.name,
                             "Grade" : self.grade 
                         }
-                    print("Appending")
-                    # First we load existing data into a dict.
-                    jsonStr = json.load(infile)
-                    jsonStr[self.test].append(dict)
+                        print("Appending")
+                        jsonStr[self.test].append(dict)
+                    else : 
+                        jsonStr.update(dict)
+                        infile.seek(0)
                     print(jsonStr)
                     jsonStr =  json.dumps(jsonStr)
             with open("sample.json", "w") as outfile: 
@@ -58,7 +60,7 @@ class Test :
     def toData(self):
         data = [self.name, self.nbQuestions]
         return data
-    def write_json(self,table,filename='sample.json'):
+    def write_json(self,table='Test',filename='sample.json'):
         print("writing")
         dict = {
             table : [{
@@ -79,72 +81,23 @@ class Test :
                     jsonStr = dict 
                     jsonStr =  json.dumps(jsonStr)
                 else: #il faudra tester ici si la tale existe deja ou pas 
-                    if table in dict :
+                    jsonStr = json.load(infile)
+                    if table in jsonStr :
                         print("The table",table,"is in the dictionnary")
                         dict = {
                             "Name" : self.name,
                             "nbQuestions" : self.nbQuestions
                         }
-
-                    print("Appending")
-                    # First we load existing data into a dict.
-                    jsonStr = json.load(infile)
-                    jsonStr[table].append(dict)
+                        print("Appending")
+                        jsonStr[table].append(dict)
+                    else :
+                        jsonStr.update(dict)
+                        infile.seek(0)                        
                     print(jsonStr)
                     jsonStr =  json.dumps(jsonStr)
             with open("sample.json", "w") as outfile: 
                 outfile.write(jsonStr) 
     
-
-# ce test fonctionne 
-# if "Test 1" in dict :
-#     print("is present")
-# else :
-#     print("not present")
-
-
-
-
-
-#il faut que cette fonction prenne en compte que le json existe mais soit vide aussi 
-#on peut aussi check si la table existe deja, et la creer si ce n'est pas le cas
-#au lieu d'avoir result comme nom de table j'aimerais que ca soit le nom du test
-def write_json(data,table,filename='sample.json'):
-    print("writing")
-    dict = {
-        table : [{
-        "Name" : data[0],
-        "Grade" : data[1]  
-        }]
-    }
-    if path.isfile(filename) is False: #on creer le json s'il n'existe pas  
-        print("Creating json")
-        jsonStr =  json.dumps(dict)
-        with open("sample.json", "w") as outfile: 
-            outfile.write(jsonStr)
-    else :
-        with open(filename,'r+') as infile:
-            #on verifie si le json est vide ou pas 
-            if path.getsize(filename) == 0 :
-                print("The file is empty: ")
-                jsonStr = dict 
-                jsonStr =  json.dumps(jsonStr)
-            else: #il faudra tester ici si la tale existe deja ou pas 
-                if table in dict :
-                    print("The table",table,"is in the dictionnary")
-                    dict = {
-                        "Name" : data[0],
-                        "Grade" : data[1]  
-                    }
-                print("Appending")
-                # First we load existing data into a dict.
-                jsonStr = json.load(infile)
-                jsonStr[table].append(dict)
-                print(jsonStr)
-                jsonStr =  json.dumps(jsonStr)
-        with open("sample.json", "w") as outfile: 
-            outfile.write(jsonStr)
-
 
 def jsonPlot(fileName,table='Test 1') :
     data = json.load(open(fileName, 'r'))
@@ -156,9 +109,7 @@ def jsonPlot(fileName,table='Test 1') :
         print ("Candidate",result['Name'],"Grade",result['Grade'])
         xAxis.append(result['Name'])
         yAxis.append(result['Grade'])
-        
-    # xAxis = [key for key, value in data.items()]
-    # yAxis = [value for key, value in data.items()]
+
 
     plot.grid(True)
     print(xAxis)
@@ -178,22 +129,3 @@ def jsonPlot(fileName,table='Test 1') :
     print("la moyenne de ce test est de : " ,average)
     plot.axhline(y=average, color ='r',linestyle='-')
     plot.show()
-
-
-# y = ["Omar",5]
-# x = ["man",3]
-# p = ["patrick",9]
-# o = ["pat",18]
-
-# omar = Candidat("Omar",15,"Test 1")
-# omar.write_json()
- 
-
-# #print(y)
-# write_json(y,"Test 1")
-# write_json(x,"Test 1")
-# write_json(p,"Test 1")
-# write_json(o,"Test 2")
-
-#jsonPlot("sample.json")
-
