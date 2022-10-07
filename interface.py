fe8cbbd28dcfd720823879bf9159bc3f7b6ef763
Omar El Chamaa <omarchamaalak@gmail.com*>
2022-10-07 16:08:40 +0200

    implementation gui quasi finit

---
+++ interface.py
@@ -0,0 +1,83 @@
+import sys
+import tkinter as tk
+import logging
+
+logging.basicConfig(filename='logFile.log', encoding='utf-8', level=logging.DEBUG)
+
+fields = 'First Name', 'Last Name', 'Repo Link', 'Repo Name', 'Candidate File', \
+         'Test File', 'Database (json)'
+
+
+class Interface(tk.Tk):
+    def __init__(self, root, return_fields=[]):
+        self.root = root
+        root.title("CapCode")
+        ents = self.createFields(fields)
+        self.bind('<Return>', (lambda event, e=ents: self.getUserInput(e)))
+        b1 = tk.Button(self, text='OK',
+                       command=(lambda e=ents: self.getUserInput(e)))
+        b1.pack(side=tk.LEFT, padx=5, pady=5)
+        b2 = tk.Button(self, text='Quit', command=self.quit)
+        b2.pack(side=tk.LEFT, padx=5, pady=5)
+        # on utilisera return fields pour stocker les infos rentrees pour pouvoir les utilsier dans main
+        self.return_fields = return_fields
+
+    def showMessage(self,message):
+        master = tk.Tk()
+        msg = tk.Message(master, text=message)
+        msg.config(bg='red', font=('times', 24, 'italic'))
+        msg.pack()
+        tk.mainloop()
+
+    def getArgs(self):
+        return self.return_fields
+
+    def getUserInput(self, entries):
+        for entry in entries:
+            field = entry[0]
+            text = entry[1].get()
+            print(type(field))
+            print(type(text))
+            if text == "":
+                logging.debug(field + "is empty ")
+                self.showMessage("PLease fill in all fields")
+                exit(1)
+
+        firstName = entries[0][1].get()
+        logging.debug('first name is ' + firstName)
+
+        lastName = entries[1][1].get()
+        logging.debug('lastName is ' + lastName)
+
+        repoLink = entries[2][1].get()
+        logging.debug('repoLink is ' + repoLink)
+
+        repoName = entries[3][1].get()
+        logging.debug('repoName is ' + repoName)
+
+        solutionFile = entries[4][1].get()
+        logging.debug('solutionFile is ' + solutionFile)
+
+        testFile = entries[5][1].get()
+        logging.debug('testFile is ' + testFile)
+
+        database = entries[6][1].get()
+        logging.debug('database is ' + database)
+
+        commandLineArg = ".\\" + testFile + " " + firstName + lastName + " " + database + " " + testFile
+        logging.debug("Command line arg is : %s", commandLineArg)
+
+        self.return_fields = [commandLineArg, repoLink, repoName, solutionFile]
+
+    def createFields(self, fields):
+        entries = []
+        for field in fields:
+            row = tk.Frame(self)
+            lab = tk.Label(row, width=15, text=field, anchor='w')
+            ent = tk.Entry(row)
+            row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+            lab.pack(side=tk.LEFT)
+            ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+            entries.append((field, ent))
+        return entries
+
--- main.py
+++ main.py
@@ -1,38 +1,52 @@
-import os 
+import os
+from tkinter import Tk 
 import git 
+from interface import Interface
 import argparse
 import logging
 import sys 
-
+import wx
+ 
 
 def main() -> int : 
 
-      #init parser 
-   parser = argparse.ArgumentParser (description='Evaluate a candidate.')
+   #init parser 
+   # parser = argparse.ArgumentParser (description='Evaluate a candidate.')
 
-   parser.add_argument('repoLink',metavar = 'repoLink',type = str ,help="Valid link to candidate git repo")
-   parser.add_argument('repoName',metavar = 'repoName',type = str ,help="Valid name of the candidate git repo")
-   parser.add_argument('candidateName',metavar ='candidateName',type = str ,help="Candidate name")
-   parser.add_argument('solutionFile',metavar ='solutionFile',type = str ,help="Name of the candidates solution file")
-   parser.add_argument('testFile'    ,metavar = 'testFile',type = str,help="Name of the test file")
-   parser.add_argument('database',metavar = 'database',type = str ,help="Name of new or existing database")
+   # parser.add_argument('repoLink',metavar = 'repoLink',type = str ,help="Valid link to candidate git repo")
+   # parser.add_argument('repoName',metavar = 'repoName',type = str ,help="Valid name of the candidate git repo")
+   # parser.add_argument('candidateName',metavar ='candidateName',type = str ,help="Candidate name")
+   # parser.add_argument('solutionFile',metavar ='solutionFile',type = str ,help="Name of the candidates solution file")
+   # parser.add_argument('testFile'    ,metavar = 'testFile',type = str,help="Name of the test file")
+   # parser.add_argument('database',metavar = 'database',type = str ,help="Name of new or existing database")
 
-   logging.basicConfig(filename='logFile.log', encoding='utf-8', level=logging.DEBUG)
+   # logging.basicConfig(filename='logFile.log', encoding='utf-8', level=logging.DEBUG)
 
-   args = parser.parse_args()
+   # args = parser.parse_args()
 
-   repoLink = args.repoLink
-   repoName = args.repoName
-   candidateName = args.candidateName
-   solutionFile = args.solutionFile
-   testFile = args.testFile
-   database = args.database 
+   # repoLink = args.repoLink
+   # repoName = args.repoName
+   # candidateName = args.candidateName
+   # solutionFile = args.solutionFile
+   # testFile = args.testFile
+   # database = args.database 
+   
+   args = []
+   root = Tk()
+   gui = Interface(root,args)
+   root.mainloop()
+
 
    #on utilise les liens ssh pour clone les projets des candidats 
    # repoLink = 'git@github.com:thrichert/capcode-candidate.git'
    # repoName = 'capcode-candidate'
 
-   commandLineArg =".\\" +testFile  + " " + candidateName + " " + database + " " + testFile  
+   # commandLineArg =".\\" +testFile  + " " + candidateName + " " + database + " " + testFile  
+   commandLineArg = args[0]
+   repoLink = args[1]
+   repoName = args[2]
+   solutionFile = args[3]
+
    logging.debug("Command line arg is : %s", commandLineArg)
 
    repo = git.Repo.clone_from(repoLink,repoName)
