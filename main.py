67aa2fb95d6ec86ddb76f94dc7418f5bc507691b
omar el Chamaa <chamaael@etu.unistra.fr>
2022-10-12 13:31:06 +0200

    Push test 2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-from tkinter import Tk
 import git
 from interface import Interface
 import argparse
@@ -12,21 +11,30 @@ import sys
 def main() -> int:
     logging.basicConfig(filename='logFile.log', encoding='utf-8', level=logging.DEBUG)
     logging.debug("in main ")
+
     parser = argparse.ArgumentParser (description='Evaluate a candidate.')
-    parser.add_argument('useType', metavar='useType', type=int,help="0 for terminal 1 for graphical interface, if 1 no need for args in terminal")
-    parser.add_argument('repoLink',metavar = 'repoLink',type = str ,help="Valid link to candidate git repo")
-    parser.add_argument('repoName',metavar = 'repoName',type = str ,help="Valid name of the candidate git repo")
-    parser.add_argument('candidateName',metavar ='candidateName',type = str ,help="Candidate name")
-    parser.add_argument('solutionFile',metavar ='solutionFile',type = str ,help="Name of the candidates solution file")
-    parser.add_argument('testFile'    ,metavar = 'testFile',type = str,help="Name of the test file")
-    parser.add_argument('database',metavar = 'database',type = str ,help="Name of new or existing database")
+    #So the user can choose to have a gui or not, when gui is enabled, ignore all other arguments if there are any
+    subparser = parser.add_subparsers(dest='option')
+
+    term = subparser.add_parser('--term')
+
+    term.add_argument('--gui',action = 'store_true',help="Enable a graphical interface for input")
+    term.add_argument('repoLink',metavar = 'repoLink',type = str ,help="Valid link to candidate git repo")
+    term.add_argument('repoName',metavar = 'repoName',type = str ,help="Valid name of the candidate git repo")
+    term.add_argument('candidateName',metavar ='candidateName',type = str ,help="Candidate name")
+    term.add_argument('solutionFile',metavar ='solutionFile',type = str ,help="Name of the candidates solution file")
+    term.add_argument('testFile'    ,metavar = 'testFile',type = str,help="Name of the test file")
+    term.add_argument('database',metavar = 'database',type = str ,help="Name of new or existing database")
+
     args = parser.parse_args()
+
+
 
     useType = args.useType
     logging.debug("Use type value : " + useType)
 
 
-    if (useType == 0):
+    if (args.option == '--term'):
         logging.debug("terminal mode ")
         repoLink = args.repoLink
         repoName = args.repoName
